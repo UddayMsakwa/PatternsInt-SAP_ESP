f8089123bb214d6b12b25a620f40f29487665d1e
Udday Msakwa <uddaymsakwa@gmail.com>
2026-06-15 22:35:05 +0700

    Implement event sourcing store

---
+++ src/Shared.Contracts/EventSourcing/EventStoreRecord.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared.Contracts.EventSourcing;
+
+public sealed class EventStoreRecord
+{
+    public Guid Id { get; set; }
+
+    public string AggregateType { get; set; } = string.Empty;
+
+    public Guid AggregateId { get; set; }
+
+    public string EventType { get; set; } = string.Empty;
+
+    public string EventData { get; set; } = string.Empty;
+
+    public DateTime CreatedAtUtc { get; set; }
+}
